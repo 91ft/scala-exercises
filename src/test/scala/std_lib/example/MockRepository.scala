41c546c774913c5f557e87aadcb1b08db8828913
karellen <karellen.kim@daumkakao.com>
2016-09-26 13:43:30 +0900

    임시 주석처리

--- src/test/scala/std_lib/example/MockRepository.scala
+++ src/test/scala/std_lib/example/MockRepository.scala
@@ -8,13 +8,13 @@ import java.util.{Calendar, Date}
 
 class MockPageRepository {
 
-  def getMD(): Page = Page(100, 100, new Date())
+  def getMD(): Page = ??? //Page(100, 100, new Date())
 
-  def getProducts(n : Int): List[Page] =
-    (10 to 10 + n).map(id => Page(id, id - 10, new Date())).toList
+  def getProducts(n : Int): List[Page] = ???
+    //(10 to 10 + n).map(id => Page(id, id - 10, new Date())).toList
 }
 
 class MockISoolHttpClient {
-  def getItems(ids : Seq[Int]): List[Item] =
-    ids.filter(id => id != 0).map(id => Item(id, if (id == 100) "MD" else s"상품${id}", s"이미지${id}")).toList
+  def getItems(ids : Seq[Int]): List[Item] = ???
+    //ids.filter(id => id != 0).map(id => Item(id, if (id == 100) "MD" else s"상품${id}", s"이미지${id}")).toList
 }
